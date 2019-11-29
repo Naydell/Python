a52c654f710799c719f705e6f09e4cf838a2ccfb
Naydell <58305031+Naydell@users.noreply.github.com>
2019-11-28 22:30:58 -0300

    Add files via upload
    Programa que lê a largura e a altura de uma parede em metros, calcula a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.

---
+++ ex011.py
@@ -0,0 +1,6 @@
+l = float(input('Digite a largura da parede: '))
+a = float(input('Digite a altura da parede: '))
+aa = l * a
+qt = aa / 2
+print('A área da parede é de {:.2f}m²'.format(aa))
+print('Precisa de {:.2f}L de tinta'.format(qt))
